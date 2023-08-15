2b54b0712fd735e846e7e140118b786e952b365d
Rahul <rahulzandennavar99@gmail.com>
2023-08-15 23:28:46 +0530

    async

---
+++ 27_aynch.js
@@ -0,0 +1,31 @@
+// console.log("This")
+// console.log("Is")
+// console.log("Sync")
+
+// Async
+
+// console.log("This")
+
+
+
+
+function loadingData( ){
+    console.log("1)Loading Data...")
+}
+
+function collectingData( ){
+    console.log("2)Collecting Data...")
+}
+
+function approvingData( ){
+    console.log("3)approving Data...")
+}
+
+function approve( ){
+    console.log("4)approved Data...")
+}
+
+loadingData( )
+collectingData( )
+approvingData( )
+approve( )
